c84505ff6fe141152bd4885fc800665d0a21f17c
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2020-08-18 16:17:01 +0300

    Тест запрос на fetch постов

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './apolloClient'
+import { useQuery, gql } from '@apollo/client'
+
+interface Post {
+  _id: number
+  heading: string
+  text: string
+  created: number
+  photoUrl: string
+}
+
+interface PostsData {
+  posts: Post[]
+}
+
+const GET_POSTS = gql`
+  query GetPosts {
+    posts {
+      _id
+      heading
+      text
+    }
+  }
+`
 
 const App = () => {
+  const { loading, error, data} = useQuery<PostsData>(GET_POSTS)
   return (
-    <ApolloProvider client={client}>
+    <>
       <h1>Hello world!</h1>
-    </ApolloProvider>
+      { error ? (<p>{error.message}</p>) : <b>No Errors</b>}
+      { loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data && data.posts.map(post => (
+            <li>
+              <h3>{post.heading}</h3>
+              <p>{post.text}</p>
+              <i>{post._id}</i>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
