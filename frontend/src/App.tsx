b937dd667793bcf0c3a40c260ea20364ef0a8c99
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2020-08-18 15:29:22 +0300

    Подключил apollo-client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apolloClient'
 
 const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <h1>Hello world!</h1>
-    </>
+    </ApolloProvider>
   );
 }
 
