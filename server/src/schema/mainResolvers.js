e1df34403b4318a2be0df7e26dc0cf6acfb2af2e
Artyom <sheludeshev.artyom@mail.ru>
2020-11-08 21:25:06 +0300

    Refactored ES6 syntax

--- server/src/schema/mainResolvers.js
+++ server/src/schema/mainResolvers.js
@@ -16,18 +16,9 @@ const resolvers = {
   Date: new GraphQLScalarType({
     name: 'Date',
     description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value) // value from the client
-    },
-    serialize(value) {
-      return value.getTime() // value sent to the client
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return parseInt(ast.value, 10) // ast value is always in string format
-      }
-      return null
-    },
+    parseValue: value => new Date(value), // value from the client
+    serialize: value => value.getTime(), // value sent to the client
+    parseLiteral: ast => ast.kind === Kind.INT ? parseInt(ast.value, 10) : null // ast value is always in string format
   })
 }
 
