06b55271a8cd0a0c5b7157eeac7d400daca3e3eb
Artyom <sheludeshev.artyom@mail.ru>
2020-11-21 21:33:58 +0300

    Added FileType into Schema

--- server/src/schema/mainSchema.js
+++ server/src/schema/mainSchema.js
@@ -8,6 +8,12 @@ const typeDefs = gql`
     success: Boolean!
     message: String!
   }
+
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
 `
 
 module.exports = typeDefs
