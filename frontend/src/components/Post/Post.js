59d437da1e1ed8100355be3d466626d1121e76ae
Artyom <sheludeshev.artyom@mail.ru>
2021-08-22 16:20:54 +0300

    Начал работать над стилями для постов

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import styles from './Post.module.css'
-import img from '../../img/Rectangle 13.png'
 
-const Post = () => {
+const Post = ({ heading, text, src, created, filter }) => {
   return (
     <div className={`${styles.post} card`}>
       <div>
-        <h2 className={styles.heading}>Cambridgeshire interactive</h2>
-        <p className={styles.paragraph}>Voluptate ex rerum praesentium nobis. Doloremque incidunt rerum. Modi voluptatem sit quas.</p>
-        <p className={styles.paragraph}>Et nostrum quia fugiat in commodi iure esse. Vel animi consequatur. Dolor fugit molestiae. Vel architecto</p>
-        <p className={styles.paragraph}>Sit eveniet sed ipsa vitae. Qui placeat quo maiores ea quia atque ut. Sint eum voluptas praesentium. Omnis libero nam officiis totam impedit totam non. Sed esse repudiandae temporibus excepturi.</p>
+        <h2 className={styles.heading}>{heading}</h2>
+        <p className={styles.paragraph}>{text}</p>
       </div>
       <div className={styles.br}></div>
       <div className={styles.right}>
-        <img src={img} alt="img" className={styles.img}/>
+        <img src={src} alt="img" className={styles.img}/>
         <button type="button" className={styles.button}>Подробнее</button>
       </div>
     </div>
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 import Post from '../Post/Post'
 import styles from './Posts.module.css'
 
+const GET_POSTS = gql`
+  query GetPosts {
+    posts {
+      _id
+      heading
+      text
+      filter
+      photo {
+        filename
+      }
+      created
+    }
+  }
+`
+
 const Posts = () => {
+  const { loading, error, data } = useQuery(GET_POSTS)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
   return (
     <div className={styles.posts}>
-      <Post />
-      <Post />
-      <Post />
+      {data.posts.map(el => (
+        <Post
+          key={el._id}
+          src={`http://localhost:4000/static/img/${el.photo.filename}`}
+          created={el.created}
+          filter={el.filter}
+          heading={el.heading}
+          text={el.text} />
+      ))}
     </div>
   )
 }
