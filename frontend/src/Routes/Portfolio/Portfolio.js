8bf5655eb6e387383c1ea0e8991067d35780e30f
Artyom <sheludeshev.artyom@mail.ru>
2021-08-26 17:02:55 +0300

    Зафетчил портфолио с сервера

--- frontend/src/Routes/Portfolio/Portfolio.js
+++ frontend/src/Routes/Portfolio/Portfolio.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 import PortfolioCard from '../../components/PortfolioCard/PortfolioCard'
 import styles from './Portfolio.module.css'
 
+const GET_PORTFOLIO = gql`
+  query GetPortfolio {
+    works {
+      _id
+      heading
+      text
+      photo {
+        filename
+      }
+      created
+    }
+  }
+`
+
 const Portfolio = () => {
+  const { loading, error, data } = useQuery(GET_PORTFOLIO)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
   return (
     <>
       <h2 className={styles.heading}>Мои работы</h2>
       <div className={styles.works}>
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
+        {data.works.map(el => (
+          <PortfolioCard
+            key={el._id}
+            src={`http://localhost:4000/static/img/${el.photo.filename}`}
+            created={el.created}
+            heading={el.heading}
+            text={el.text}
+            />
+        ))}
       </div>
     </>
   )
--- frontend/src/components/PortfolioCard/PortfolioCard.js
+++ frontend/src/components/PortfolioCard/PortfolioCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './PortfolioCard.module.css'
-import img from '../../img/gallery/5.jpg'
 
-const PortfolioCard = () => {
+const PortfolioCard = ({src, heading, created, text}) => {
+  const date = new Date(created).toLocaleDateString()
   return (
     <div className={styles.card}>
-      <img className={styles.card__img} src={img} alt="work"/>
+      <img
+        className={styles.card__img}
+        src={src}
+        alt="work"/>
       <div className={styles.card__info}>
-        <h3 className={styles.card__heading}>Веб-сайт</h3>
-        <span className={styles.card__date}>02.10.2020</span>
-        <p className={styles.card__text}>Voluptatibus itaque repellat eos cum molestias et consequatur laudantium asperiores.</p>
+        <h3 className={styles.card__heading}>{heading}</h3>
+        <span className={styles.card__date}>{date}</span>
+        <p className={styles.card__text}>{text}</p>
         <button className={styles.card__button} type="button">Подробнее</button>
       </div>
     </div>
